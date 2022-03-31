e9ccfcc8edaea43ac0ca931e2d7dd9844a0a542b
Mirzobek <94282513+qmirzobek@users.noreply.github.com>
2022-03-31 14:06:34 +0500

    Update Pre-release Material.py

--- Pre-release Material.py
+++ Pre-release Material.py
@@ -2,11 +2,10 @@ midday=[]
 midnight=[]
 count=0
 temp=0
-count1=0
 total=0
 total2=0
-#this sectioon for input valid temperatures in 5 day
-for count in range(0,5):
+#this sectioon for input valid temperatures in 7 day
+for count in range(0,7):
     midday.append(0)
     midnight.append(0)
     print(("Please input temperature of midday in"),int(count+1),("day"))
@@ -26,19 +25,18 @@ for count in range(0,5):
 print(("This is midday temperature which are entered"),midday)
 print(("This is midnight temperature which are entered"),midnight)
 #this section for find avarage of temperature of midday and midnight
-for count1 in range(0,4):
-    total=total+midday[count1]
-    total2=total2+midnight[count1]
-print(("This is avarage temperature of middays in 5 days:"),total/4)
-print(("This is avarage temperature of midnights in 5 days:"),total2/4)
+for count in range(0,7):
+    total=total+midday[count]
+    total2=total2+midnight[count]
+print(("This is avarage temperature of middays in 7 days:"),total/7)
+print(("This is avarage temperature of midnights in 7 days:"),total2/7)
 #this section for find highest temperature of midday and midnight
 highday=midday[0]
 highnight=midnight[0]
-for count in range(0,4):
+for count in range(0,7):
     if highday<midday[count]:
         highday=midday[count]
     if highnight<midnight[count]:
         highnight=midnight[count]
-print(highday, highnight)
-print(("This is highest temperature of midday in 5 days"),highday)
-print(("This is highest temperature of midnight in 5 days"),highnight)
+print(("This is highest temperature of midday in 7 days"),highday)
+print(("This is highest temperature of midnight in 7 days"),highnight)
